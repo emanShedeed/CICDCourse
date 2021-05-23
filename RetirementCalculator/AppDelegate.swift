70af255b23698e6e04f0310a4075ee3adfcdc332
emanShedeed <42905983+emanShedeed@users.noreply.github.com>
2021-05-23 16:10:17 +0200

    add new key

--- RetirementCalculator/AppDelegate.swift
+++ RetirementCalculator/AppDelegate.swift
@@ -17,7 +17,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
-        AppCenter.start(withAppSecret: "613a7765-6bc2-43eb-82ac-b57b5aa5c663", services:[
+        AppCenter.start(withAppSecret: "613a7765-6bc2-43eb-82ac-5f69e303-8638-48b2-a5fd-4b3557217016", services:[
             Crashes.self,
             Analytics.self
         ])
